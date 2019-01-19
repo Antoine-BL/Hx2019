70d644c811971387327a35c2306678f85ae06bee
Charles Piette <charliesis>
2019-01-19 17:35:24 -0500

    gets

--- server/server.py
+++ server/server.py
@@ -1,13 +1,18 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/api/signup")
+@app.route("/api/signup", methods=['get'])
 def signup():
-    #bullshit
+    return 'signup'
+
+@app.route("/api/google", methods=['get'])
+def google():
+    return 'google'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
