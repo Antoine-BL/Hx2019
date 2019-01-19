b379cc543839b818e5e0b4db0627d1150326a037
Charles Piette <charliesis>
2019-01-19 18:02:04 -0500

    boi

--- server/server.py
+++ server/server.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from os import getenv
 import json
+import pypyodbc
+
 app = Flask(__name__)
+
+
+connection = pypyodbc.connect('Driver={SQL Server};Server=bdallovelo.c4hn4wmypuno.us-east-2.rds.amazonaws.com;Database=DBO;uid=PolyHxAlloVelo;pwd=2chocolats')
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/api/signup", methods=['get'])
+@app.route("/api/signup", methods=['PUT'])
 def signup():
     return 'signup'
 
