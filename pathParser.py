1519801402789af55b7c44379a9dd8d4e39f9f68
Charles Piette <charliesis>
2019-01-19 15:56:45 -0500

    rounded the coords

--- pathParser.py
+++ pathParser.py
@@ -10,9 +10,11 @@ class pathData :
             data = json.load(file)
             for feature in data['features']:
                 for line in feature['geometry']['coordinates']:
-                    self.mapData[(line[0][0], line[0][1])].append({'id': feature['properties']['ID'], 'endPoint': (line[-1][0], line[-1][1]), 'path': line})
-                    self.mapData[(line[-1][0], line[-1][1])].append({'id': feature['properties']['ID'], 'endPoint': (line[0][0], line[0][1]), 'path': line})
+                    self.mapData[self.roundedCoord(line[0][0:2])].append({'id': feature['properties']['ID'], 'endPoint': self.roundedCoord(line[-1][0:2]), 'path': line})
+                    self.mapData[self.roundedCoord(line[-1][0:2])].append({'id': feature['properties']['ID'], 'endPoint': self.roundedCoord(line[0][0:2]), 'path': line})
 
+    def roundedCoord(self, coord):
+        return (round(coord[0], 5), round(coord[1], 5))
 
     def findClosestPath(self, coord):
 
@@ -56,6 +58,7 @@ class pathData :
             if nextPath == finalPoint :
                 break
             else :
+                print(path)
                 path.clear()
         return path
         
@@ -63,6 +66,7 @@ class pathData :
         
     
 map = pathData('montreal.json')
-point1 = (-73.96219789981842, 45.41612831487927)
-point2 = (-73.9369797706604, 45.44953407110633)
-print (map.makePath(point1, point2))
+point1 = map.roundedCoord([-73.96219789981842, 45.41612831487927])
+point2 = map.roundedCoord([-73.9369797706604, 45.44953407110633])
+point3 = map.roundedCoord([-73.83718013763428, 45.42866626130953])
+print (map.makePath(point1, point3))
