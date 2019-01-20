a2e3049f53602a6d5efe0ecfb8164e99528187b0
Charles Piette <charliesis>
2019-01-19 23:25:32 -0500

    users

--- server/server.py
+++ server/server.py
@@ -1,10 +1,51 @@
-from flask import request
-from flask import Flask, render_template
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, request
 from os import getenv
 import json
 import googleApi
 
 app = Flask(__name__, static_folder = "./dist", template_folder = ".")
+app.secret_key = 'super secret string'  # Change this!
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    with open('./database/users.json', 'r+') as file:
+        isGood = False
+        data = json.load(file)
+        for item in data :
+            if item['email'] == email:
+                isGood = True
+        if not isGood:
+            return
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.json['email']
+    with open('./database/users.json', 'r+') as file:
+        isGood = False
+        data = json.load(file)
+        for item in data :
+            if item['email'] == request.json['email'] and item['password'] == request.json['password']:
+                isGood = True
+        if not isGood:
+            return
+
+    user = User()
+    user.id = email
+    user.is_authenticated = True
+
+    return user
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -35,13 +76,28 @@ def login():
     email = content['email']
     password = content['password']
 
+    with open('./database/users.json', 'r') as file:
+        data = json.load(file)
+        for user in data:
+            if user['email'] == email and user['password'] == password :
+                user = User()
+                user.id = email
+                login_user(user)
+                return 'yup'#redirect(url_for('protected'))
     
-    return "login"
+    return "invalid"
+
+
+@app.route('/protected')
+@login_required
+def protected():
+    return 'Logged in as: ' + current_user.id
 
 #logout
 @app.route("/api/logout")
 def logout():
-  print ('a')
+    logout_user()
+    return 'Logged out'
 
 @app.route("/api/google/<string:lat>/<string:lon>", methods=['GET'])
 def google(lat, lon):
