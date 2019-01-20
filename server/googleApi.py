bcaa8c908eac13a66c6a471a80c3872329bbc0ce
Charles Piette <charliesis>
2019-01-19 19:30:55 -0500

    google api

--- server/googleApi.py
+++ server/googleApi.py
@@ -1,13 +1,25 @@
 import requests
 import polyline
+import json
 
 #https://maps.googleapis.com/maps/api/directions/json?origin=45.44289515059061,-73.76473903656006&destination=45.43191889517022,-73.6774706840515&mode=bicycling&key=AIzaSyCBhoFmnGT3UQVAvedNxPGaOfpdmJBmDLM&fbclid=IwAR1B8UIEizZTmD1TlrMbEeI0gd6j47RBdNNDm1zlKv4InPlOCuCKvkArwAk%27
 
 class googleApi :
     def __init__(self, start, end):
         self.resp = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + start + '&destination=' + end + '&mode=bicycling&key=AIzaSyCBhoFmnGT3UQVAvedNxPGaOfpdmJBmDLM&fbclid=IwAR1B8UIEizZTmD1TlrMbEeI0gd6j47RBdNNDm1zlKv4InPlOCuCKvkArwAk')
+        self.json = self.resp.json()
+    
+    def getDistance(self):
+        return self.json['routes'][0]['legs'][0]['distance']['value']
+    
+    def getData(self):
+        return json.dumps({
+            'distance': self.getDistance(),
+            'path': polyline.decode(self.json['routes'][0]['legs'][0]['steps'][1]['polyline']['points'])
+        })
+
 
 google = googleApi('45.44289515059061,-73.76473903656006', '45.43191889517022,-73.6774706840515')
 print (google.resp.json())
 
-print (polyline.decode(google.resp.json()['routes'][0]['legs'][0]['steps'][1]['polyline']['points']))
+print (google.getData())
--- server/server.py
+++ server/server.py
@@ -2,11 +2,11 @@ from flask import Flask
 from os import getenv
 import json
 import pypyodbc
+import googleApi
 
 app = Flask(__name__)
 
-
-connection = pypyodbc.connect('Driver={SQL Server};Server=bdallovelo.c4hn4wmypuno.us-east-2.rds.amazonaws.com;Database=DBO;uid=PolyHxAlloVelo;pwd=2chocolats')
+#connection = pypyodbc.connect("Driver=SQL Server;Server=bdallovelo.c4hn4wmypuno.us-east-2.rds.amazonaws.com;Database=DBO;uid=PolyHxAlloVelo;pwd=2chocolats")
 
 @app.route("/")
 def hello():
@@ -16,9 +16,9 @@ def hello():
 def signup():
     return 'signup'
 
-@app.route("/api/google", methods=['get'])
-def google():
-    return 'google'
+@app.route("/api/google/<string:lat>/<string:lon>", methods=['GET'])
+def google(lat, lon):
+    return googleApi.googleApi(lat, lon).getData()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
